fc096fa4daa4049be0a788a777d149697ffef0e8
itsAdee <adeelahmedqureshi02@gmail.com>
2023-05-10 18:24:03 +0500

    added the directory functionalities

--- FileManagementSystem.py
+++ FileManagementSystem.py
@@ -21,6 +21,25 @@ class FileManagementSystem:
         self.files[name] = file
         self.current_directory.add_file(file)
 
+    def create_directory(self, name, log):
+        directoryPath = name.split("/")
+        current_directory = self.current_directory
+        for i in directoryPath:
+            if i == "":
+                continue
+            for directory in current_directory.subdirectories:
+                if directory.name == i:
+                    current_directory = directory
+                    break
+        if directoryPath[-1] == "":
+            directory = Directory(directoryPath[-2])
+        else:
+            directory = Directory(directoryPath[-1])
+        log.write("Directory Creation Successful")
+
+        current_directory.add_subdirectory(directory)
+        self.subdirectories[name] = directory
+
     def delete_file(self, name, log):
         current_directory = self.current_directory
         for file in current_directory.files:
@@ -30,6 +49,26 @@ class FileManagementSystem:
                 return
             else:
                 log.write("File Deletion Failed: File does not exist")
+
+    def delete_directory(self, directoryName, log):
+        try:
+            directoryPath = directoryName.split("/")
+
+            current_directory = self.current_directory
+            for i in directoryPath:
+                if i == "":
+                    continue
+                for directory in current_directory.subdirectories:
+                    if directory.name == i:
+                        current_directory = directory
+                        break
+            for file in current_directory.files:
+                file.delete(self.Memory)
+            current_directory.parent.subdirectories.remove(current_directory)
+            log.write("Directory Deletion Successful")
+
+        except:
+            print("directory not found")
 
     def append_file(self, name, data, log):
         try:
@@ -79,13 +118,78 @@ class FileManagementSystem:
             for block in file.blocks:
                 blocks += str(block) + ","
             log.write(" " * spaces + file.name + " " + str(file.id) +
-                  " " + str(file.file_size) + " " + blocks)
+                      " " + str(file.file_size) + " " + blocks)
         for directory in current_directory.subdirectories:
             if directory.name == self.current_directory.name:
                 log.write(" " * spaces + "*" + directory.name + "/")
             else:
                 log.write(" " * spaces + directory.name + "/")
             self.memoryMap(directory, log)
+
+    def change_directory(self, name, log):
+        if self.current_directory.parent == None and name == "..":
+            return
+
+        if name == "..":
+            self.current_directory = self.current_directory.parent
+            log.write("directory changed to " +
+                      str(self.current_directory.name))
+        else:
+            pathToDirectory = name.split("/")
+            for name in pathToDirectory:
+                if name == "":
+                    continue
+                for directory in self.current_directory.subdirectories:
+                    if directory.name == name:
+                        self.current_directory = directory
+                        log.write("directory changed to " +
+                                  str(self.current_directory.name))
+                        break
+
+    def moveFileInDirectory(self, fileName, newDirectory, log):
+        try:
+            current_directory = self.current_directory
+            my_file = current_directory.find_file(fileName)
+            current_directory.remove_file(my_file)
+        except:
+            log.write("File Move Failed: File does not exist")
+            return
+
+        if newDirectory == "..":
+            if current_directory.parent == None:
+                return
+
+            current_directory = current_directory.parent
+            duplicate = current_directory.find_file(fileName)
+            if duplicate != None:
+                current_directory.remove_file(duplicate)
+                log.write("Duplicate file found, deleting duplicate")
+            current_directory.add_file(my_file)
+            return
+
+        newFileDirectory = newDirectory.split("/")
+        for i in newFileDirectory:
+            if i == "":
+                continue
+            for directory in current_directory.subdirectories:
+                if directory.name == i:
+                    current_directory = directory
+                    break
+        duplicate = current_directory.find_file(fileName)
+        if duplicate != None:
+            current_directory.remove_file(duplicate)
+            log.write("Duplicate file found, deleting duplicate")
+        current_directory.add_file(my_file)
+
+    def passWorkingDirectory(self):
+        current_directory = self.current_directory
+        path = ""
+        while current_directory != None and current_directory.parent is not None:
+            path = "/" + current_directory.name + path
+            current_directory = current_directory.parent
+        if path == "":
+            path = "/"
+        return path
 
     def save(self):
         with open("file_system.pickle", "wb") as file:
@@ -116,13 +220,13 @@ class FileManagementSystem:
         elif command == "touch":
             self.create_file(prompt[1], log)
             log.write("Created file: " + prompt[1])
-        
+
         # Delete a file (remove from the directory): rm <filename>
         elif command == "rm":
             self.delete_file(prompt[1], log)
             log.write("Deleted file: " + prompt[1])
             pass
-        
+
         # Write to a file (overwrite): wr <filename> <text>
         elif command == "wr":
             written_text = ""
@@ -130,7 +234,7 @@ class FileManagementSystem:
                 written_text += prompt[i] + " "
             self.write_file(prompt[1], written_text, log)
             log.write("Wrote to file: " + prompt[1])
-        
+
         # Append to a file (add to the end): ap <filename> <text>
         elif command == "ap":
             appended_text = ""
@@ -138,12 +242,12 @@ class FileManagementSystem:
                 appended_text += prompt[i] + " "
             self.append_file(prompt[1], appended_text, log)
             log.write("Appended to file: " + prompt[1])
-        
+
         # Truncate a file (remove content): tr <filename> <end bit>
         elif command == "tr":
             self.truncate_file(prompt[1], prompt[2], log)
             log.write("Truncated file: " + prompt[1])
-        
+
         # Move content within a file: mvc <filename> <start> <size> <newstart/target>
         elif command == "mvc":
             self.MoveContent(prompt[1], int(
@@ -156,7 +260,7 @@ class FileManagementSystem:
             log.write(self.readFile(prompt[1], log))
             log.write("File read")
             pass
-        
+
         elif command == "mmap":
             log.write("\nLogging Memory Map:")
             self.memoryMap(log=log)
@@ -167,4 +271,3 @@ class FileManagementSystem:
         elif command == "save":
             self.save()
             log.write("Saved file system")
-        
--- Main.py
+++ Main.py
@@ -19,9 +19,10 @@ def create_file_system():
 
     file_system.save()
 
+
 def createUserThread(user_name):
     user = User(user_name)
-    user.runCommands(f"user_commands/{user_name}_input.txt", file_system)    
+    user.runCommands(f"user_commands/{user_name}_input.txt", file_system)
 
 
 if __name__ == "__main__":
@@ -33,14 +34,31 @@ if __name__ == "__main__":
         create_file_system()
 
     # Create multiple users on different threads
-    users = ["user1", "user2", "user3"]
+    input = input("Enter the number of threads to create: ")
+    input = int(input)
+    users = []
+    for i in range(input):
+        users.append(f"user{i}")
     threads = []
     for user in users:
         t = threading.Thread(target=createUserThread, args=(user,))
         threads.append(t)
         t.start()
-    
     for t in threads:
         t.join()
-    
+
+    print("Saving file system...\n")
+    print("File system saved.\n")
+    file_system.save()
+
+    # users = ["user1", "user2", "user3"]
+    # threads = []
+    # for user in users:
+    #     t = threading.Thread(target=createUserThread, args=(user,))
+    #     threads.append(t)
+    #     t.start()
+
+    # for t in threads:
+    #     t.join()
+
     # file_system.save()
